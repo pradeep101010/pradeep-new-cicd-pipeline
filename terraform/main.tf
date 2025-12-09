b88b78d15425440d42b2f4765a72017bdd87ead1
pradeepsn10101 <pradeepsn10101@gmail.com>
2025-12-09 16:13:23 +0530

    changed az version

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,10 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.0"
+      version = ">= 3.110"
     }
   }
-  required_version = ">= 1.3.0"
+  required_version = ">= 1.6.0"
 }
 
 provider "azurerm" {

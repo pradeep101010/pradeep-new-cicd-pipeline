fedbf23854280de3025c19a3f9768dc5bb0dc246
pradeepsn10101 <pradeepsn10101@gmail.com>
2025-12-09 16:18:23 +0530

    changed ver again

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,10 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.110"
+      version = ">= 3.0"
     }
   }
-  required_version = ">= 1.6.0"
+  required_version = ">= 1.3.0"
 }
 
 provider "azurerm" {
